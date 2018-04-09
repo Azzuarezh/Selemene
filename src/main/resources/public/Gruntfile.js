da20424275e0003a7deb9a4c06fe2aef90ac1973
Azzuarezh <spotlight.magic@gmail.com>
2018-04-09 13:34:01 +0700

    remove watch task in grunt

--- src/main/resources/public/Gruntfile.js
+++ src/main/resources/public/Gruntfile.js
@@ -36,6 +36,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-wiredep');
 
-  grunt.registerTask('default', ['wiredep','watch']);
+  grunt.registerTask('default', ['wiredep']);
 
 };
