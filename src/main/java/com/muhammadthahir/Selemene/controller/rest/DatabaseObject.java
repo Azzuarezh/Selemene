008a307a8152866a49993f4727a51ff75e998232
Azzuarezh <spotlight.magic@gmail.com>
2018-05-09 17:58:03 +0700

    remove unused import package (mockito)

--- src/main/java/com/muhammadthahir/Selemene/controller/rest/DatabaseObject.java
+++ src/main/java/com/muhammadthahir/Selemene/controller/rest/DatabaseObject.java
@@ -3,11 +3,11 @@
  */
 package com.muhammadthahir.Selemene.controller.rest;
 
-import static org.mockito.Matchers.contains;
+
 
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
